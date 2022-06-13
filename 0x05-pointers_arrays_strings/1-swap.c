9124a057ea211365ece3bbb2ac92d75ea2871d49
csjo7eth <csjo.1515@gmail.com>
2022-06-13 05:05:50 -0700

    strlen

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,10 +3,20 @@
 /**
  * swap_int - swap the values o two intigers
  *
+ * @a - intiger to be swaped
+ * @b - intiger to be swaped
+ *
  * Return: Always (0);
  */
 
 void swap_int(int *a, int *b);
 {
+	int *a = 98;
+	int *b = 42;
+
+	printf("a= %d and b= %d", *a, *b);
 	swap_int(&a, &b);
+	printf("a= %d and b= %d", *a, *b);
+
+	return (0);
 }
