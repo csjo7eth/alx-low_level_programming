2dabc3333a8ac3d7c4a52e40a605efdf99f31bb3
csjo7eth <csjo.1515@gmail.com>
2022-06-20 05:39:29 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memset - fill mimory with constant byte
+ *
+ * @s - pointer to the memory
+ * @n - byte of memory
+ * @b - constant byte
+ *
+ * Return: the memory area of s
+ *
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
