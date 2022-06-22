c412af2be048604c2b0f69a115fb661b4d999530
csjo7eth <csjo.1515@gmail.com>
2022-06-22 01:27:19 -0700

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - a function which prints string
+ *
+ * @s - a pointer of string
+ *
+ *Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	s++;
+	_puts_recursion(s)
+	
+	return;
+}
