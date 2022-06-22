289537f7ba0aa5cd298a2733f8058c32d696db2e
csjo7eth <csjo.1515@gmail.com>
2022-06-22 02:22:05 -0700

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Calculate the length of a string
+ * @s: the string to count
+ *
+ * Return: integer value
+ */
+
+int _strlen_recursion(char *s)
+{
+	int cou = sizeof(*s);
+
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_putchar(cou);
+		s++;
+		_strlen_recursion(s);
+	}
+}
