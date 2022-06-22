18602c120c7d1857049943f74e9718d8f136efcd
csjo7eth <csjo.1515@gmail.com>
2022-06-22 02:06:42 -0700

    reverse recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Print a string in reverse
+ * @s: the string to reverse
+ *
+ * Return: Nothing.
+ */
+
+void print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		s++;
+		print_rev_recursion(s);
+		s--;
+		_putchar(*s);
+	}
+}
