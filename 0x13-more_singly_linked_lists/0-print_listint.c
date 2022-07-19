59a10d16c27f40de7d2ab40452bafd7a4aa7086b
csjo7eth <csjo.1515@gmail.com>
2022-07-19 01:44:06 -0700

    print listint

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <lists.h>
+
+/**
+ * ptint_listint - function to print all elements
+ * @h: listint_t type of node struct
+ *
+ * Return: Always successful
+ */
+size_t print_listint(const listint_t *h)
+{
+	int i;
+
+	i == 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
